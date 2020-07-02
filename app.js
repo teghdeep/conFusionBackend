0c0efaebd674c171dfab170395e9648c073183a5
Teghdeep Kapoor <teghdeep@gmail.com>
2020-07-03 04:19:30 +0530

    Assignmnet 4

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
 var passport = require("passport");
 var authenticate = require("./authenticate");
 var config = require("./config");
@@ -13,17 +15,13 @@ var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
 const uploadRouter = require("./routes/uploadRouter");
-
-var app = express();
-var session = require("express-session");
-var FileStore = require("session-file-store")(session);
+const favoriteRouter = require("./routes/favoriteRouter");
 
 const mongoose = require("mongoose");
 
 const Dishes = require("./models/dishes");
 
 const url = config.mongoUrl;
-
 const connect = mongoose.connect(url);
 
 connect.then(
@@ -35,37 +33,7 @@ connect.then(
   }
 );
 
-//app.use(cookieParser("12345-67890-09876-54321"));
-
-app.use(
-  session({
-    name: "session-id",
-    secret: "12345-67890-09876-54321",
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore(),
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
-function auth(req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-    var err = new Error("You are not authenticated!");
-    err.status = 403;
-    next(err);
-  } else {
-    next();
-  }
-}
-
-app.use(auth);
+var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -74,6 +42,12 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser('958-9148-441'));
+
+app.use(passport.initialize());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -81,7 +55,7 @@ app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
 app.use("/imageUpload", uploadRouter);
-
+app.use("/favorites", favoriteRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
